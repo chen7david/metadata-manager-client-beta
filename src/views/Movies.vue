27d96d8dfafa54c8ccbae7b89f2fc47a6bb727df
David <chen7david@me.com>
2020-06-28 12:30:37 +0800

    update

---
+++ src/views/Movies.vue
@@ -0,0 +1,23 @@
+  
+<template>
+    <div>Movies</div>
+</template>
+
+<script>
+import { mapGetters, mapState } from 'vuex'
+export default {
+    name:'Movies',
+    data: () => ({}),
+    components: {
+    },
+    computed: {
+        ...mapGetters([
+            
+        ])
+    },
+    methods: {
+        ...mapState([
+        ])
+    },
+}
+</script>
---
+++ src/views/Shows.vue
@@ -0,0 +1,23 @@
+  
+<template>
+    <div>Shows</div>
+</template>
+
+<script>
+import { mapGetters, mapState } from 'vuex'
+export default {
+    name:'Shows',
+    data: () => ({}),
+    components: {
+    },
+    computed: {
+        ...mapGetters([
+            
+        ])
+    },
+    methods: {
+        ...mapState([
+        ])
+    },
+}
+</script>
